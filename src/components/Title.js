ab92e755bb9d40671ee50f60f68d5fca9a0715ec
ivan <ivanruch@gmail.com>
2019-01-22 16:55:32 +0100

    Optimize for mobile

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,10 +1,23 @@
 import React, {Component} from 'react';
+import ReactTooltip from 'react-tooltip'
+import Description from './Description';
+import Legend from './Legend';
 
 class Title extends Component {
   render() {
     return (
       <div id="title">
-        <h3>MiNI Korzenie</h3>
+        <div>
+          <h3>MiNI Korzenie</h3>
+          <a data-tip id="tooltip" data-for='tooltip-body'>?</a>
+          <ReactTooltip id='tooltip-body' place="bottom" type="dark" effect="solid">
+            <div id='tooltip-text'>
+              <p>Drzewo genealogicznie pokazujące związki między współczesnymi pracownikami wydziału Matematyki i Nauk Informacyjnych Politechniki Warszawskiej  a Warszawską i Lwowską szkołą matematyczną. Szkoły te razem z Krakowską szkołą matematyczną tworzą  polską szkołę matematyczną, czyli środowisko matematyków działających w Polsce w latach 1918-1939.</p>
+              <p>Fakt, że osoba jest potomkiem innej osoby w drzewie oznacza, że była jej doktorantem/doktorantką.</p>
+              <Legend/>
+            </div>
+          </ReactTooltip>
+        </div>
       </div>
     );
   }
